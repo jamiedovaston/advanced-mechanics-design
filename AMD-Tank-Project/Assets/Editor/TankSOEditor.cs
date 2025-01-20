88da1e649b1e46fdb02e92a565a015cdc0d0eab0
Jamie Dovaston <jamiedovaston@outlook.com>
2025-01-20 12:25:31 +0000

    Changes to Tank editor

--- AMD-Tank-Project/Assets/Editor/TankSOEditor.cs
+++ AMD-Tank-Project/Assets/Editor/TankSOEditor.cs
@@ -6,23 +6,19 @@ public class TankSOEditor : Editor
 {
     public override void OnInspectorGUI()
     {
-        // Check if the target object has a "BannerImage" field
         SerializedProperty bannerImageProperty = serializedObject.FindProperty("BannerImage");
         if (bannerImageProperty != null)
         {
             if (bannerImageProperty.objectReferenceValue != null)
             {
-                // Get the banner image
                 Texture2D bannerImage = (Texture2D)bannerImageProperty.objectReferenceValue;
 
                 if (bannerImage)
                 {
                     float aspectRatio = (float)bannerImage.height / bannerImage.width;
 
-                    // Get the current Inspector width
                     float inspectorWidth = EditorGUIUtility.currentViewWidth;
 
-                    // Draw the banner image
                     GUILayout.BeginHorizontal();
                     GUILayout.FlexibleSpace();
                     GUILayout.Label(bannerImage, GUILayout.Width(inspectorWidth - 40), GUILayout.Height((inspectorWidth - 40) * aspectRatio));
@@ -32,9 +28,9 @@ public class TankSOEditor : Editor
             }
         }
 
-        // Draw the rest of the default inspector
         EditorGUILayout.Space();
-        serializedObject.ApplyModifiedProperties(); // Apply changes to serialized fields
+        serializedObject.ApplyModifiedProperties();
+
         base.OnInspectorGUI();
     }
 }
--- AMD-Tank-Project/Assets/_FRAMEWORK/02-TankController/Scripts/Types/TankBASESO.cs
+++ AMD-Tank-Project/Assets/_FRAMEWORK/02-TankController/Scripts/Types/TankBASESO.cs
@@ -2,5 +2,7 @@
 
 public abstract class TankBASESO : ScriptableObject
 {
+#if UNITY_EDITOR
     public Texture2D BannerImage;
+#endif
 }
